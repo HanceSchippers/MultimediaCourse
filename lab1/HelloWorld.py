35d816a6fea3a0378febb0ed23c00e1ae33e83a4
HanceSchippers <hanceschippers@gmail.com>
2025-03-20 17:24:39 +0800

    delete some unused python file

--- lab1/HelloWorld.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def calculate_a(d, beta):
-    # Ensure beta is in radians
-    a =  d * math.sin(math.radians(beta))*2/3
-    return a
-
-# Example usage
-print(math.sin(math.radians(5)))
